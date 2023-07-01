749d758f2391758098662b116174c76c11942eca
mikenitj <mikenitipong@hotmail.com>
2023-07-01 21:46:22 +0700

    add readme description

---
+++ js/app.js
@@ -0,0 +1,20 @@
+$.ajax("https://sugoku.onrender.com/board?difficulty=easy")
+.then((data)=> {
+    console.log(data)
+})
+
+$.ajax("https://sugoku.onrender.com/solve")
+.then((data2)=> {
+    console.log(data2)
+})
+
+console.log("Mike")
+
+// https://animechan.xyz/api/random
+
+
+// https://github.com/bertoort/sugoku
+
+// https://cataas.com/cat
+
+
